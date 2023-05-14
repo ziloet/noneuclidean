7fb3920bd48c314038fde2585f067d583c8480e9
Lazuli Ziloet <rneutron@protonmail.com>
2023-05-14 18:12:58 +0300

    created a window

--- main_win32.c
+++ main_win32.c
@@ -1,5 +1,8 @@
 #include<windows.h>
 
+#define Assert(Condition) if(Condition); else __debugbreak()
+
+
 static void
 disable_dpi_scaling(void)
 {
@@ -21,7 +24,36 @@ disable_dpi_scaling(void)
 	}
 }
 
+static LRESULT CALLBACK
+WindowProc(HWND Window, UINT Message, WPARAM wParam, LPARAM lParam)
+{
+	LRESULT Result = DefWindowProcW(Window, Message, wParam, lParam);
+	return Result;
+}
+
+static HWND
+create_window(void)
+{
+	HINSTANCE Instance = GetModuleHandleW(0);
+	WCHAR* WindowClassName = L"wndclass_name";
+	WNDCLASSEXW WindowClass = {0};
+	WindowClass.cbSize = sizeof(WindowClass);
+	WindowClass.lpszClassName = WindowClassName;
+	WindowClass.lpfnWndProc = WindowProc;
+	WindowClass.hInstance = Instance;
+	RegisterClassExW(&WindowClass);
+	DWORD WindowStyle = WS_OVERLAPPED | WS_VISIBLE;
+	RECT WindowRect = {0, 0, 1280, 720};
+	AdjustWindowRect(&WindowRect, WindowStyle, 0);
+	LONG WindowWidth = WindowRect.right - WindowRect.left;
+	LONG WindowHeight = WindowRect.bottom - WindowRect.top;
+	HWND Result = CreateWindowExW(0, WindowClassName, L"Non-Euclidian", WindowStyle, 0, 0, WindowWidth, WindowHeight, 0, 0, Instance, 0);
+	Assert(IsWindow(Result));
+	return Result;
+}
+
 void WinMainCRTStartup()
 {
 	disable_dpi_scaling();
+	HWND Window = create_window();
 }
